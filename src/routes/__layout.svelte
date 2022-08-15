2b98d3a0895b4c17fd6aaa3aceca3aae8b7e43dc
Jonas Kruckenberg <iterpre@protonmail.com>
2022-08-15 10:56:33 +0200

    bring back plots

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 1280px;
+		margin: 0 auto;
+		padding: 2rem;
+		text-align: center;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const moduli = [...Array(500).keys()];
+</script>
+<section class="circles">
+	{#each moduli as modulus}
+				<div class="circle">
+					{modulus + 1}
+					<img
+						src={`circleplot://localhost?modulus=${modulus + 1}&stroke=${stroke}`}
+						alt={`Circle plot for the Pisano period with modulus ${modulus}`}
+					/>
+				</div>
+	{/each}
+</section>
+
+<style>
+	.circles {
+		width: 85vw;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+		gap: 3.5rem;
+	}
+
+	.circle {
+		text-align: start;
+		font-family: Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace;
+	}
+</style>
